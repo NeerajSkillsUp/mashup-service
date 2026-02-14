a085322a68093923514108ce90fb2ff796573613
Neeraj Bhardwaj <thisis.neerajbhardwaj@gmail.com>
2026-02-14 19:20:55 +0530

    Update 102317014.py

--- 102317014.py
+++ 102317014.py
@@ -1,15 +1,6 @@
-import sys, os, time, platform, importlib
+import sys, os, time, platform
 from yt_dlp import YoutubeDL
 from pydub import AudioSegment
-
-if platform.system() == 'Windows':
-    FFMPEG_BIN = r'C:\ffmpeg\bin'
-    os.environ["PATH"] += os.pathsep + FFMPEG_BIN
-    AudioSegment.converter = os.path.join(FFMPEG_BIN, "ffmpeg.exe")
-    AudioSegment.ffprobe = os.path.join(FFMPEG_BIN, "ffprobe.exe")
-else:
-    AudioSegment.converter = "ffmpeg"
-    AudioSegment.ffprobe = "ffprobe"
 
 def run_mashup(singer, n, y, out_file):
     unique_id = str(int(time.time()))
@@ -20,7 +11,6 @@ def run_mashup(singer, n, y, out_file):
         'nocheckcertificate': True,
         'outtmpl': f'temp_{unique_id}_%(id)s.%(ext)s',
         'max_downloads': n,
-        # THE FIX: Force the Android client which YouTube trusts more than the Web client
         'extractor_args': {'youtube': {'player_client': ['android']}},
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
@@ -36,7 +26,7 @@ def run_mashup(singer, n, y, out_file):
     files = [f for f in os.listdir() if f.startswith(f"temp_{unique_id}_") and f.endswith(".mp3")]
     
     if not files:
-        raise Exception("YouTube blocked the request. Please try again in 5 minutes.")
+        raise Exception("YouTube blocked the request. Try again in 5 minutes or use a different singer name.")
 
     mashup = AudioSegment.empty()
     for f in files:
@@ -48,7 +38,3 @@ def run_mashup(singer, n, y, out_file):
 
     mashup.export(out_file, format="mp3")
     return out_file
-
-if __name__ == "__main__":
-    if len(sys.argv) == 5:
-        run_mashup(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]), sys.argv[4])
